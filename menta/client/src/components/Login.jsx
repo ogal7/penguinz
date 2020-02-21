3133d5643012a27632b62220955b3b7d9d1a03a6
Margot <margaretsobota@gmail.com>
2020-02-20 19:44:20 -0600

    added react components for login/register and router

--- menta/client/src/components/Login.jsx
+++ menta/client/src/components/Login.jsx
@@ -5,16 +5,50 @@ import "../style/login.css";
 
 
 class Login extends Component {
+	constructor ()
+	{
+		super();
+		this.state={
+			email: "",
+			password: "",
+			errors: {}
+		};
+	}
+
+	onChange = e => {
+    	this.setState({ [e.target.id]: e.target.value });
+  	};
+
+  	onSubmit = e => {
+    	e.preventDefault();
+
+    	const userData = {
+      	email: this.state.email,
+      	password: this.state.password
+      };
+    };
+
 	render(){
+		const {errors} = this.state;
 		return(
 	    	<div class="container">
 	            <div class="form-container">
-	        		<form>
+	        		<form noValidate onSubmit={this.onSubmit}>
 	                    <h1 class="login-header"> Login </h1>
 	        			<div class= "login-input">
-	        				<input type="text" placeholder="Enter Email" name="email" required />
-	        				<input type="password" placeholder="Enter Password" name="pass" required />
-	        				<button id= "login-button" type="submit" value= "dashboard.html">Login</button>
+	        				<input 
+	        					onChange= {this.onChange}
+	        					type="text" 
+	        					placeholder="Enter Email" 
+	        					name="email" 
+	        					required />
+	        				<input 
+	        					onChange= {this.onChange}
+	        					type="password" 
+	        					placeholder="Enter Password" 
+	        					name="pass" 
+	        					required />
+	        				<button id= "login-button" type="submit">Login</button>
 	        				<p> Don't have an account? <Link to="/register">Register</Link>. </p>
 	        			</div>
 	        		</form>
