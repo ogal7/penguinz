4033f0cf1768be972865fef9fe82d47f20061e4b
Margot <margaretsobota@gmail.com>
2020-02-23 14:46:52 -0600

    added files for redux

--- menta/client/src/App.js
+++ menta/client/src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import { Login, Register } from './components'
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
