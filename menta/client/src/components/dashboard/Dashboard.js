6d96659705222202a47fee25f38e6d207750e3fe
Margot <margaretsobota@gmail.com>
2020-02-29 14:40:28 -0600

    finished auth but unable to test b/c MongoDB not working

---
+++ menta/client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+class Dashboard extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+render() {
+    const { user } = this.props.auth;
+return (
+      <div className="container">
+        <h4>
+          <b>Hey there,</b> {user.name.split(" ")[0]}
+        </h4>
+        <button
+          style={{
+            width: "150px",
+            borderRadius: "3px",
+            letterSpacing: "1.5px",
+            marginTop: "1rem"
+          }}
+          onClick={this.onLogoutClick}
+        >
+          Logout
+        </button>
+      </div>
+    );
+  }
+}
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
